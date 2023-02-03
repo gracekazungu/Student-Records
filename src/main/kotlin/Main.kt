16c462995b446c58febc013406795c03e823d8f4
gracekazungu <kazungungrace@gmail.com>
2023-02-03 07:25:47 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var fullName: String = "Grace Neema"
+    println(fullName)
+    var age: Byte = 20
+    println(age)
+    var phoneNumber: String = "0746404458"
+    println(phoneNumber)
+    var weightKg = 60
+    println(weightKg)
+    var citizenKenyan: Boolean = true
+    println(citizenKenyan)
+}
